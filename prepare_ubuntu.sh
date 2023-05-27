bbc71465cd7b22a310a78df786c8a1895d6b69b4
ViFigueiredo <67883343+ViFigueiredo@users.noreply.github.com>
2023-05-27 19:31:26 -0300

    Update prepare_ubuntu.sh

--- prepare_ubuntu.sh
+++ prepare_ubuntu.sh
@@ -17,13 +17,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 
 # Instalação do tema Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Instalação do Fzf
 yes | ~/.fzf/install
 
 # Alteração do tema no arquivo ~/.zshrc
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+# sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 
 # Substituição dos plugins no arquivo ~/.zshrc
 sed -i 's/plugins=(git)/plugins=( git  zsh-syntax-highlighting fzf zsh-autosuggestions k )/' ~/.zshrc
@@ -42,4 +42,4 @@ nvm use v18.16.0
 zsh
 
 # Respostas às perguntas do Powerlevel10k
-echo -e "n\nn\nn\nn\n2\n3\n2\n2\n2\n2\n2\ny\n1\ny\ny"
+# echo -e "n\nn\nn\nn\n2\n3\n2\n2\n2\n2\n2\ny\n1\ny\ny"
