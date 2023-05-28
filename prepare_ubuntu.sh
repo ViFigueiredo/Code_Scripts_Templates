4bee23a3749097e17a1e7c0cf695b359496243a2
ViFigueiredo <67883343+ViFigueiredo@users.noreply.github.com>
2023-05-27 22:29:22 -0300

    Update prepare_ubuntu.sh

--- prepare_ubuntu.sh
+++ prepare_ubuntu.sh
@@ -4,26 +4,22 @@
 sudo apt update
 sudo apt upgrade -y
 
-# Instalação do Zsh e dependências
+# Zsh e Fonts
 sudo apt-get install zsh fonts-powerline -y
+zsh
+chsh -s $(which zsh)
 
-# Instalação do Oh My Zsh
+# Oh My Zsh
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
-# Instalação de plugins do Zsh
+# Plugins Zsh
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
-
-# Instalação do tema Powerlevel10k
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Instalação do Fzf
 yes | ~/.fzf/install
-
-# Alteração do tema no arquivo ~/.zshrc
-# sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 
 # Substituição dos plugins no arquivo ~/.zshrc
 sed -i 's/plugins=(git)/plugins=( git zsh-syntax-highlighting fzf zsh-autosuggestions k )/' ~/.zshrc
@@ -38,9 +34,4 @@ source ~/.nvm/nvm.sh
 nvm install v18.16.0
 nvm use v18.16.0
 
-# Iniciar o shell Zsh
-zsh
-chsh -s $(which zsh)
 
-# Respostas às perguntas do Powerlevel10k
-# echo -e "n\nn\nn\nn\n2\n3\n2\n2\n2\n2\n2\ny\n1\ny\ny"
