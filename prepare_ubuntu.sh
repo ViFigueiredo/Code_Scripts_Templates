65aeb75effa4f31355a10f2d7599efd89f813ef8
Vi Figueiredo <vifigueiredo@outlook.com.br>
2024-06-10 16:19:54 -0300

    "Updated prepare_ubuntu.sh script to add installation of software-properties-common, wget, curl, and python3-launchpadlib, and to install pyenv; modified zsh installation and configuration"

--- prepare_ubuntu.sh
+++ prepare_ubuntu.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Sistema, fontes e terminal zsh
+# Sistema, pacotes e ppas
 sudo apt update
 sudo apt upgrade -y
+sudo apt install software-properties-common wget curl python3-launchpadlib -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
 
 # Git
 git config --global user.name "Vi Figueiredo"
 git config --global user.email vifigcode@gmail.com.br
 
 # Terminal ZSH e Plugins
-sudo apt-get install zsh -y
-sudo apt-get install fonts-powerline -y
+sudo apt-get install zsh fonts-powerline -y
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && yes | ~/.fzf/install
@@ -18,8 +20,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
 sed -i 's/plugins=(git)/plugins=( git docker zsh-syntax-highlighting fzf zsh-autosuggestions k )/' ~/.zshrc
 echo "dev" | chsh -s $(which zsh) && exec zsh
+source ~/.zshrc
 
 # NVM e Node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install --lts
+
+# Pyenv
+curl https://pyenv.run | bash
+source ~/.zshrc
